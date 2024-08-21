14fd86666ba94ca53f1dc0b79cc54bbf9fa99491
omid nami <omid.nami.110@gmail.com>
2024-08-22 00:34:28 +0330

    remove js file on app dir

--- app/Error.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ErrorSqlDesible = void 0;
-const ErrorSqlDesible = () => { return "mysql Desibled in env file"; };
-exports.ErrorSqlDesible = ErrorSqlDesible;
