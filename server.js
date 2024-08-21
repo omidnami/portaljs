4edfcb6d3ee82c3d4d6fe1a5488bf1501237dd7f
omid nami <omid.nami.110@gmail.com>
2024-08-22 00:40:47 +0330

    remove all js files

--- server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./configs/app");
-app_1.App.listen((process.env.PORT), () => {
-    console.log("Server is Running by : " + process.env.PROJECT_NAME + " port: " + process.env.PORT);
-    // console.log('The solution is: ', JSON.stringify(msql));
-});
