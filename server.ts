f1169a7e545de50f57b4f2b9bdfa81e4cc090845
omid nami <omid.nami.110@gmail.com>
2024-08-24 00:29:07 +0330

    socket app maked

--- server.ts
+++ server.ts
@@ -1,8 +1,17 @@
 import { App } from "./configs/app"
 import { StartQueue } from "./configs/queue"
+var http = require('http');
+import Socket from "./app/SocketApp";
+const server = http.createServer(App); 
 
+// socket connection on portocol ws or wss
+if (process.env.SOCKET === "true"){
+    Socket.connect(server)
+    console.log('socket enabled');
+}
 
-App.listen((process.env.PORT),()=>{
+// server conect on portocol http or https
+App.listen((process.env.PORT || 3008),()=>{
     StartQueue()
     console.log("Server is Running by : "+ process.env.PROJECT_NAME +" port: "+process.env.PORT)
     // console.log('The solution is: ', JSON.stringify(msql));
